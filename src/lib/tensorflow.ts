6857a086998cbcb0e5efbe3af2073e5b2c834a43
bal16 <ahmadhamidbalya@gmail.com>
2025-12-30 11:59:10 +0700

    style: clean up comment

--- src/lib/tensorflow.ts
+++ src/lib/tensorflow.ts
@@ -2,7 +2,6 @@ import * as tfCore from '@tensorflow/tfjs-core';
 import * as tfLayers from '@tensorflow/tfjs-layers';
 import '@tensorflow/tfjs-backend-wasm';
 
-// Gabungkan Core dan Layers ke satu objek 'tf' biar mirip import default
 const tf = {
   ...tfCore,
   ...tfLayers,
@@ -12,8 +11,3 @@ export type { LayersModel } from '@tensorflow/tfjs-layers';
 export type { Tensor } from '@tensorflow/tfjs-core';
 
 export default tf;
-
-//Cannot find namespace 'tf'.ts(2503)
-// ⚠ Error (TS2503)  |  |  |
-
-// Cannot find namespace tf .
