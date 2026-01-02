2346c2fd36f399d3c275cd248a15863ce2acaf3d
bal16 <ahmadhamidbalya@gmail.com>
2026-01-02 14:29:52 +0700

    fix: remove unused request handler from index.ts

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,6 @@ import { createApp } from '../src/lib/app';
 
 const app = createApp();
 
-export default async function handler(request: Request) {
-  logger.info(`${request.method} ${request.url}`);
-  return app.fetch(request);
-}
+logger.info('Starting the BeatWell API server...');
+
+export default app;
